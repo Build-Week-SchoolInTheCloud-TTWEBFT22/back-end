f784655aac60da1c2fae799d5c9153a1efe24b58
kai-blt <blaitong@gmail.com>
2020-12-21 11:00:03 -0800

    added a tasks controller

--- src/main/java/com/lambdaschool/schoolinthecloud/controllers/UserTasksController.java
+++ src/main/java/com/lambdaschool/schoolinthecloud/controllers/UserTasksController.java
@@ -1,27 +1,29 @@
-//package com.lambdaschool.schoolinthecloud.controllers;
-//
-//import com.lambdaschool.schoolinthecloud.models.User;
-//import com.lambdaschool.schoolinthecloud.services.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping(name = "/tasks")
-//public class UserTasksController {
-//    @Autowired
-//    UserService userService;
-//
-//    // http://localhost:2019/volunteers/volunteers
-//    @GetMapping(name = "/volunteers/", produces = "application/json")
-//    public ResponseEntity<?> findAllVolunteers() {
-//        List<User> userList = userService.findAll();
-//
-//        return new ResponseEntity<>(userList, HttpStatus.OK);
-//    }
-//}
+package com.lambdaschool.schoolinthecloud.controllers;
+
+import com.lambdaschool.schoolinthecloud.models.User;
+import com.lambdaschool.schoolinthecloud.models.UserTasks;
+import com.lambdaschool.schoolinthecloud.services.UserService;
+import com.lambdaschool.schoolinthecloud.services.UserTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class UserTasksController {
+    @Autowired
+    UserTaskService userTaskService;
+
+    // http://localhost:2019/tasks/tasks
+    @GetMapping(value = "/tasks", produces = "application/json")
+    public ResponseEntity<?> findAllTasks() {
+        List<UserTasks> userList = userTaskService.findAll();
+
+        return new ResponseEntity<>(userList, HttpStatus.OK);
+    }
+}
