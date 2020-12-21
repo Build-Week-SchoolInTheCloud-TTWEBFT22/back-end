5263d97459304ca1e7052571f9a216532edfcd53
kai-blt <blaitong@gmail.com>
2020-12-21 13:15:51 -0800

    added findtaskbyid endpoint and add task endpoint

--- src/main/java/com/lambdaschool/schoolinthecloud/controllers/UserTasksController.java
+++ src/main/java/com/lambdaschool/schoolinthecloud/controllers/UserTasksController.java
@@ -4,11 +4,17 @@ import com.lambdaschool.schoolinthecloud.models.User;
 import com.lambdaschool.schoolinthecloud.models.UserTasks;
 import com.lambdaschool.schoolinthecloud.services.UserService;
 import com.lambdaschool.schoolinthecloud.services.UserTaskService;
+import io.swagger.models.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -26,11 +32,41 @@ public class UserTasksController {
         return new ResponseEntity<>(userList, HttpStatus.OK);
     }
 
+    // GET http://localhost:2019/tasks/task/{taskid}
+    // Get specific task using the  provided taskid
+    @GetMapping(value = "/task/{taskid}", produces = "application/json")
+    public ResponseEntity<?> getTaskById(@PathVariable long taskid) {
+        UserTasks task = userTaskService.findUsertasksById(taskid);
+        return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
     // DELETE http://localhost:2019/tasks/task/{taskid}
     // Removes the task with the provided taskid
     @DeleteMapping(value = "/task/{taskid}", produces = "application/json")
-    public ResponseEntity<?> findAllTasks(@PathVariable long taskid) {
+    public ResponseEntity<?> deleteTask(@PathVariable long taskid) {
         userTaskService.delete(taskid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    // POST http://localhost:2019/tasks/task/{userid}/description/{taskdescription}
+    @PostMapping(value = "/task/{userid}/description/{taskdescription}", consumes = "application/json")
+    public ResponseEntity<?> addTask(
+        @PathVariable long userid,
+        @PathVariable String taskdescription) {
+
+        UserTasks newTask = userTaskService.save(userid, taskdescription);
+
+        // set the location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newUserURI = ServletUriComponentsBuilder.fromCurrentServletMapping()
+                .path("/tasks/task/{taskid}")
+                .buildAndExpand(newTask.getTaskid())
+                .toUri();
+        responseHeaders.setLocation(newUserURI);
+
+        return new ResponseEntity<>(null,
+                responseHeaders,
+                HttpStatus.CREATED);
+    }
+
 }
